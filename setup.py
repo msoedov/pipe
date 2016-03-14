097b4992552a597747ce569e7e5102b3109c50b4
Alex Myasoedov <msoedov@gmail.com>
2016-03-14 19:35:39 +0200

    Adds py package template

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pipe.py',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='msoedov',
+    author_email='msoedov@gmail.com',
+    description='Pipe library for python'
+)
